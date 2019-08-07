7867fd2a3917db9a076f72d79e6b605a1081f18a
wbluke <pwb0127@naver.com>
2019-08-07 22:37:07 +0900

    [ADD] 기본적인 scroll HeaderBar 생성

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,33 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+	  <header-bar></header-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HeaderBar from './components/HeaderBar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HeaderBar
   },
   data: () => ({
     //
---
+++ src/frontend/src/components/HeaderBar.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-card class="overflow-hidden">
+		<v-app-bar
+						absolute
+						color="white"
+						elevate-on-scroll
+						scroll-target="#scrolling-techniques-7"
+		>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			
+			<v-toolbar-title>JPA Shop</v-toolbar-title>
+			
+			<v-spacer></v-spacer>
+		
+		</v-app-bar>
+		<v-sheet
+						id="scrolling-techniques-7"
+						class="overflow-y-auto"
+						max-height="800"
+		>
+			<v-container style="height: 1500px;">
+			
+			</v-container>
+		</v-sheet>
+	</v-card>
+</template>
+
+<script>
+    export default {
+        name: "HeaderBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
