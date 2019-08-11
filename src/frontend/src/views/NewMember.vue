8d137bd485d0b8eafdfbe25f501e7ff6ecc24add
wbluke <pwb0127@naver.com>
2019-08-11 21:58:32 +0900

    [ADD] 회원가입 페이지 뷰 작성

--- src/frontend/src/views/NewMember.vue
+++ src/frontend/src/views/NewMember.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="newMember">
-		<h1>new member</h1>
+		<h1>회원 가입</h1>
+		
+		<v-flex xs12 sm6 md6 my-auto pt-6>
+			<v-text-field label="이름" placeholder="이름을 입력하세요"></v-text-field>
+			<v-text-field label="도시" placeholder="도시를 입력하세요"></v-text-field>
+			<v-text-field label="거리" placeholder="거리를 입력하세요"></v-text-field>
+			<v-text-field label="우편 번호" placeholder="우편 번호를 입력하세요"></v-text-field>
+			
+			<v-layout justify-end>
+				<v-btn fab dark small color="light-green lighten-2" class="mr-2">
+					<v-icon>done</v-icon>
+				</v-btn>
+				<v-btn fab dark small color="red lighten-2">
+					<v-icon>clear</v-icon>
+				</v-btn>
+			</v-layout>
+		</v-flex>
+	
+	
 	</div>
 </template>
 
