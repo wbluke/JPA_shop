34aab04186961f48aef35b3d78b27f47cea451e3
wbluke <pwb0127@naver.com>
2019-08-12 11:26:16 +0900

    [ADD] 상품 주문 페이지 뷰 작성

--- src/frontend/src/views/NewOrder.vue
+++ src/frontend/src/views/NewOrder.vue
@@ -1,12 +1,33 @@
 <template>
 	<div class="newOrder">
 		<h1>상품 주문</h1>
+		
+		<v-flex xs12 sm6 md6 my-auto pt-6>
+			<v-autocomplete label="주문 회원" :items="orderMembers"></v-autocomplete>
+			<v-autocomplete label="상품명" :items="items"></v-autocomplete>
+			<v-text-field label="주문 수량" placeholder="주문 수량을 입력하세요"></v-text-field>
+			
+			<v-layout justify-end>
+				<v-btn fab dark small color="light-green lighten-2" class="mr-2">
+					<v-icon>done</v-icon>
+				</v-btn>
+				<v-btn fab dark small color="red lighten-2">
+					<v-icon>clear</v-icon>
+				</v-btn>
+			</v-layout>
+		</v-flex>
 	</div>
 </template>
 
 <script>
     export default {
-        name: "NewOrder"
+        name: "NewOrder",
+        data() {
+            return {
+                orderMembers: [],
+                items: []
+            }
+        }
     }
 </script>
 
