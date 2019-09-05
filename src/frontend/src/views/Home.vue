4169351f61af990750cfb883d221fc980405401d
wbluke <pwb0127@naver.com>
2019-09-05 16:00:18 +0900

    feat: Home.vue 추가

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,6 +1,33 @@
 <template>
 	<div class="home">
-		<h1>home</h1>
+		<h1>Home</h1>
+		
+		<v-card class="my-6">
+			<v-container>
+				<v-layout>
+					<v-flex md4>
+						<v-img class="white--text" height="350px"
+							src="http://bimage.interpark.com/goods_image/5/9/5/3/240925953g.jpg" contain>
+						</v-img>
+					</v-flex>
+					<v-flex md8>
+						<v-card-text>
+							<span>In KI-Workshop</span><br>
+							<span class="text--primary font-weight-black title">
+                                <span>JPA Shop admin page for studying JPA</span>
+                            </span>
+						</v-card-text>
+						
+						<v-card-actions>
+							<v-btn href="https://github.com/wbluke/JPA_shop" text color="green">
+								Go to github
+							</v-btn>
+						</v-card-actions>
+					</v-flex>
+				</v-layout>
+			</v-container>
+		
+		</v-card>
 	
 	</div>
 </template>
