1ba10b4e2e94fbe7861b721aebd270cb3ebbf7b9
deocksoo <dusol92@snu.ac.kr>
2019-09-13 19:16:35 +0900

    feat(ch3): persist 관련 테스트 추가

--- src/test/java/study/api/test/ch3/PersistMethodTest.java
+++ src/test/java/study/api/test/ch3/PersistMethodTest.java
@@ -3,12 +3,10 @@ package study.api.test.ch3;
 import org.junit.jupiter.api.*;
 import study.api.test.model.ApiTestEntity;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PersistMethodTest {
     private static final String TEST_NAME = "test entity";
@@ -16,6 +14,7 @@ class PersistMethodTest {
     private static EntityManagerFactory emf;
 
     private EntityManager entityManager;
+    private EntityManager anotherManager;
     private ApiTestEntity apiTestEntity;
 
     @BeforeAll
@@ -26,6 +25,7 @@ class PersistMethodTest {
     @BeforeEach
     void setUp() {
         entityManager = emf.createEntityManager();
+        anotherManager = emf.createEntityManager();
         apiTestEntity = new ApiTestEntity(TEST_NAME, TEST_AGE);
     }
 
@@ -35,6 +35,41 @@ class PersistMethodTest {
         entityManager.persist(apiTestEntity);
 
         assertThat(entityManager.contains(apiTestEntity)).isTrue();
+    }
+
+    @Test
+    @DisplayName("트렌젝션 없이 엔티티를 persist하면 id가 자동생성되지 않는다.")
+    void idNotGenerated() {
+        entityManager.persist(apiTestEntity);
+
+        assertThat(apiTestEntity.getId()).isNull();
+    }
+
+    @Test
+    @DisplayName("트렌젝션이 있을 때 엔티티를 persist하면 commit 하기 전에 id가 자동생성된다.")
+    void idGenerated() {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+        assertThat(apiTestEntity.getId()).isNull();
+        entityManager.persist(apiTestEntity);
+        assertThat(apiTestEntity.getId()).isNotNull();
+        transaction.rollback();
+
+        // rollback 해도 자동생성된 id는 사라지지 않는다.
+        assertThat(apiTestEntity.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("persist하고 트렌젝션을 rollback하면 엔티티를 1차 캐시에서 지운다.")
+    void removeCacheWhenRollback() {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+        entityManager.persist(apiTestEntity);
+        transaction.rollback();
+
+        assertThat(entityManager.contains(apiTestEntity)).isFalse();
     }
 
     @Test
@@ -65,7 +100,6 @@ class PersistMethodTest {
     @Test
     @DisplayName("트렌젝션을 시작하고 엔티티를 persist하면 DB에 저장된다.")
     void persistWithTransaction() {
-        EntityManager anotherManager = emf.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         Long id;
 
@@ -81,13 +115,35 @@ class PersistMethodTest {
     @Test
     @DisplayName("트렌젝션을 시작하지 않고 엔티티를 persist 하면 DB에는 저장되지 않는다.")
     void persistWithoutTransaction() {
-        EntityManager anotherManager = emf.createEntityManager();
         Long id = 999L;
 
         entityManager.persist(apiTestEntity);
         apiTestEntity.setId(id);
 
         assertThat(anotherManager.find(ApiTestEntity.class, id)).isNull();
+    }
+
+    @Test
+    @DisplayName("flush하면 SQL 저장소의 쿼리가 DB에 전송될 뿐, DB에 변경사항이 커밋되지는 않는것 같다.")
+    void flush() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        Long id;
+
+        transaction.begin();
+        entityManager.persist(apiTestEntity);
+        id = apiTestEntity.getId();
+        entityManager.flush();
+
+        assertThat(anotherManager.find(ApiTestEntity.class, id)).isNull();
+        transaction.rollback();
+    }
+
+    @Test
+    @DisplayName("트렌젝션이 없을 때 flush하면 TransactionRequiredException이 발생한다.")
+    void flushWithoutTransaction() {
+        entityManager.persist(apiTestEntity);
+        assertThatThrownBy(() -> entityManager.flush())
+                .isInstanceOf(TransactionRequiredException.class);
     }
 
     @AfterEach
