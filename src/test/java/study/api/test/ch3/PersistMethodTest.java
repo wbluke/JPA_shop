12fdcd398d0fdc1f5cf0201e8ac1f5bc78e502d6
deocksoo <dusol92@snu.ac.kr>
2019-09-13 18:01:07 +0900

    fix: create table 이용해서 테이블 자동생성하도록 수정
    hibernate dialect를 MySQLDialect로 사용하는 경우 ddl-auto 속성이 제대로 적용되지 않아
    일일이 테이블을 생성해주었습니다. dialect를 MySQL8Dialect를 사용하면 해당 문제가 해결됩니다.
    문제가 해결되면서 PersisMethodTest에서 필요없어진 createTable 메서드와 dropTable 메서드를
    제거하였습니다.

--- src/test/java/study/api/test/ch3/PersistMethodTest.java
+++ src/test/java/study/api/test/ch3/PersistMethodTest.java
@@ -11,13 +11,11 @@ import javax.persistence.Persistence;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PersistMethodTest {
-    private static final String TABLE_NAME = "entity";
     private static final String TEST_NAME = "test entity";
     private static final Integer TEST_AGE = 28;
     private static EntityManagerFactory emf;
 
     private EntityManager entityManager;
-    private EntityTransaction transaction;
     private ApiTestEntity apiTestEntity;
 
     @BeforeAll
@@ -28,10 +26,7 @@ class PersistMethodTest {
     @BeforeEach
     void setUp() {
         entityManager = emf.createEntityManager();
-        transaction = entityManager.getTransaction();
         apiTestEntity = new ApiTestEntity(TEST_NAME, TEST_AGE);
-
-        createTable(TABLE_NAME);
     }
 
     @Test
@@ -45,6 +40,7 @@ class PersistMethodTest {
     @Test
     @DisplayName("id의 Generation Type이 identity이면 persist할 때 id가 자동 생성된다.")
     void persistWhenGenerationTypeIsIdentity() {
+        EntityTransaction transaction = entityManager.getTransaction();
         Long id;
 
         transaction.begin();
@@ -70,6 +66,7 @@ class PersistMethodTest {
     @DisplayName("트렌젝션을 시작하고 엔티티를 persist하면 DB에 저장된다.")
     void persistWithTransaction() {
         EntityManager anotherManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
         Long id;
 
         transaction.begin();
@@ -81,12 +78,11 @@ class PersistMethodTest {
         assertThat(anotherManager.find(ApiTestEntity.class, id)).isEqualTo(apiTestEntity);
     }
 
-
     @Test
     @DisplayName("트렌젝션을 시작하지 않고 엔티티를 persist 하면 DB에는 저장되지 않는다.")
     void persistWithoutTransaction() {
         EntityManager anotherManager = emf.createEntityManager();
-        Long id = 1L;
+        Long id = 999L;
 
         entityManager.persist(apiTestEntity);
         apiTestEntity.setId(id);
@@ -96,29 +92,11 @@ class PersistMethodTest {
 
     @AfterEach
     void tearDown() {
-        dropTable(TABLE_NAME);
         entityManager.close();
     }
 
     @AfterAll
     static void tearDownAfterAll() {
         emf.close();
-    }
-
-    private void createTable(String tableName) {
-        transaction.begin();
-        entityManager.createNativeQuery("CREATE TABLE " + tableName + " (" +
-                "id bigint not null auto_increment," +
-                "name varchar(255)," +
-                "age integer(255)," +
-                "primary key (id))"
-        ).executeUpdate();
-        transaction.commit();
-    }
-
-    private void dropTable(String tableName) {
-        transaction.begin();
-        entityManager.createNativeQuery("DROP TABLE " + tableName).executeUpdate();
-        transaction.commit();
     }
 }
